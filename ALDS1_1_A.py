91435e008eebcbed92e6776b0c707eb61341467b
aokht <aokht.zxc.v@gmail.com>
2016-08-01 09:55:48 -0700

    ALDS1_1_A

--- ALDS1_1_A.py
+++ ALDS1_1_A.py
@@ -13,10 +13,28 @@ def parseIntArray(array, strip = True):
 
 def parse(lines):
     return [
+        parseInt(lines[0]),
+        parseIntArray(lines[1].split(' ')),
     ]
 
+def printArray(array):
+    print(' '.join([str(x) for x in array]))
+
 def solve(args):
-    pass
+    N, A = args;
+
+    for i in range(1, len(A)):
+        printArray(A)
+
+        v = A[i]
+        j = i - 1
+        while j >= 0 and v < A[j]:
+            A[j+1] = A[j]
+            j -= 1
+
+        A[j + 1] = v
+
+    printArray(A)
 
 # {{{ main
 def main():
